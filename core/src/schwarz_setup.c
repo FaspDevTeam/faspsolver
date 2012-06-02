db2a9bfbd89a034bfc7119b6b597c51152b2faf9
Chensong Zhang <zhangchensong@gmail.com>
2012-06-03 00:00:10 +0800

    Fix Doxygen problem in schwarz_setup.

--- core/src/schwarz_setup.c
+++ core/src/schwarz_setup.c
@@ -1,10 +1,8 @@
 /*!
  *  \file schwarz_setup.c
- *  \brief setup phase for schwarz method
+ *  \brief Setup phase for the Schwarz methods
  *
  *  Created by Hu Xiaozhe on 3/22/11.
- *  Copyright 2011 PSU. All rights reserved.
- *
  */
 
 #include <math.h>
@@ -20,25 +18,27 @@
 
 /**
  * \fn INT fasp_schwarz_setup(Schwarz_data *schwarz, INT mmsize, INT maxlev, INT schwarz_type)
- * \brief setup phase for schwarz method
  *
- * \param *schwarz pointer to the shcwarz data
- * \param mmsize max block size
- * \param maxlev max level
- * \return  0 success
+ * \brief Setup phase for the Schwarz methods
+ *
+ * \param schwarz        Pointer to the shcwarz data
+ * \param mmsize         Max block size
+ * \param maxlev         Max number of levels
+ * \param schwarz_type   Type of the Schwarz method
+ *
+ * \return               SUCCESS if succeed
  *
  * \author Ludmil, Xiaozhe Hu
- * \date 03/22/2011 
+ * \date   03/22/2011 
  */
-INT fasp_schwarz_setup(Schwarz_data *schwarz, 
+INT fasp_schwarz_setup (Schwarz_data *schwarz, 
 					    INT mmsize,
 					    INT maxlev,
 					    INT schwarz_type)
 { 
- 
 	// information about A
-	dCSRmat A=schwarz->A;
-	INT n = A.row;
+	dCSRmat A = schwarz->A;
+	INT n   = A.row;
 	INT nnz = A.nnz;
 	INT *ia = A.IA;
 	INT *ja = A.JA;
@@ -60,18 +60,22 @@ INT fasp_schwarz_setup(Schwarz_data *schwarz,
 	// return 
 	INT flag = 0;
 
+#if DEBUG_MODE
+    printf("### DEBUG: fasp_schwarz_setup ...... [Start]\n");
+#endif
+
 	// allocate memory
-	maxa   = (INT *)fasp_mem_calloc(n1,sizeof(INT));
-	mask   = (INT *)fasp_mem_calloc(n1,sizeof(INT));
-	iblock   = (INT *)fasp_mem_calloc(n1,sizeof(INT));
-	jblock   = (INT *)fasp_mem_calloc(n1,sizeof(INT));
+	maxa    = (INT *)fasp_mem_calloc(n1,sizeof(INT));
+	mask    = (INT *)fasp_mem_calloc(n1,sizeof(INT));
+	iblock  = (INT *)fasp_mem_calloc(n1,sizeof(INT));
+	jblock  = (INT *)fasp_mem_calloc(n1,sizeof(INT));
 	
 	nsizeall=0;
 	
-	for (i=0;i<n1;i++){
-			mask[i]=0;
-			iblock[i]=0;
-			maxa[i]=0;
+	for (i=0;i<n1;i++) {
+        mask[i]=0;
+        iblock[i]=0;
+        maxa[i]=0;
 	}
 	
 	maxa[0]=1;
@@ -82,7 +86,7 @@ INT fasp_schwarz_setup(Schwarz_data *schwarz,
 	//! find the blocks
 	/*-------------------------------------------*/
 	// first pass
-	for (i=0;i<MIS.row;i++){
+	for (i=0;i<MIS.row;i++) {
 		// for each node do a maxlev level sets out 
 		inroot = MIS.val[i]+1;
 		levels_(&inroot,ia,ja,mask,&nlvl,maxa,jblock,&maxlev);
@@ -91,10 +95,10 @@ INT fasp_schwarz_setup(Schwarz_data *schwarz,
 	}
 
 #if DEBUG_MODE	
-	fprintf(stdout,"\nnsizeall is: %d\n",nsizeall);
+	fprintf(stdout,"### DEBUG: nsizeall is: %d\n",nsizeall);
 #endif
 		
-	/* we only calculated the size of this up to here  so we can reallocate jblock*/
+	/* We only calculated the size of this up to here. So we can reallocate jblock */
 	jblock  = (INT *)fasp_mem_realloc(jblock,(nsizeall+n)*sizeof(INT));
 	
 	// second pass
@@ -103,7 +107,7 @@ INT fasp_schwarz_setup(Schwarz_data *schwarz,
 	iblock[0]=1;
 	nsizeall=0;
 	jb=jblock;
-	for (i=0;i<MIS.row;i++){
+	for (i=0;i<MIS.row;i++) {
 		inroot = MIS.val[i]+1;
 		levels_(&inroot,ia,ja,mask,&nlvl,maxa,jb,&maxlev);
 		nsizei=maxa[nlvl]-1;
@@ -113,37 +117,39 @@ INT fasp_schwarz_setup(Schwarz_data *schwarz,
 	}
 	nblk = MIS.row;
 
-
 #if DEBUG_MODE
-	fprintf(stdout,"\nCHECK: nsizall is: %d %d\n",nsizeall,iblock[nblk]-1);
-	fprintf(stdout,"\nCHECK: nnz is: %d\n",ia[n]-1);
+	fprintf(stdout,"### DEBUG: nsizall is: %d %d\n",nsizeall,iblock[nblk]-1);
+	fprintf(stdout,"### DEBUG: nnz is: %d\n",ia[n]-1);
 #endif
 	
 	/*-------------------------------------------*/
 	//! LU decomposition of blocks
 	/*-------------------------------------------*/
 	n1 = (nblk + iblock[nblk]-1);
-	maxa   = (INT *)fasp_mem_realloc(maxa,n1*sizeof(INT));
+
+	maxa = (INT *)fasp_mem_realloc(maxa,n1*sizeof(INT));
 	
-	for(i=0;i<n1;i++) maxa[i]=0;
-	for(i=0;i<n;i++) mask[i]=0;
+	for (i=0;i<n1;i++) maxa[i]=0;
+
+	for (i=0;i<n; i++) mask[i]=0;
 	
 	// first estimate the memroy we need.
 	mxfrm2_(&n,ia,ja,&nblk,iblock,jblock,mask,maxa,&memt,&maxbs);
 	
 #if DEBUG_MODE
-	fprintf(stdout,"\nYYYNumber of nonzeroes for LU=%d maxbs=%d\n",memt, maxbs);
+	fprintf(stdout,"### DEBUG: Number of nonzeroes for LU=%d maxbs=%d\n",memt, maxbs);
 #endif
 	
 	// allocate the memory
-	al   = (REAL *)fasp_mem_calloc(memt,sizeof(REAL));
-	au   = (REAL *)fasp_mem_calloc(memt,sizeof(REAL));
-	rhsloc   = (REAL *)fasp_mem_calloc(maxbs, sizeof(REAL));
+	al     = (REAL *)fasp_mem_calloc(memt,  sizeof(REAL));
+	au     = (REAL *)fasp_mem_calloc(memt,  sizeof(REAL));
+	rhsloc = (REAL *)fasp_mem_calloc(maxbs, sizeof(REAL));
     
 	//  LU decomposition
-      sky2ns_(&n,ia,ja,a,&nblk,iblock,jblock,mask,maxa,au,al);
+    sky2ns_(&n,ia,ja,a,&nblk,iblock,jblock,mask,maxa,au,al);
 	  
-	printf("Schwarz setup success:  matrix size = %d, #blocks = %d, max block size =%d\n", n, nblk, maxbs);
+	printf("Schwarz setup succeeded: matrix size = %d, #blocks = %d, max block size =%d\n", 
+           n, nblk, maxbs);
       
 	/*-------------------------------------------*/  
 	//! return
@@ -156,17 +162,17 @@ INT fasp_schwarz_setup(Schwarz_data *schwarz,
 	schwarz->al = al;
 	
 	schwarz->schwarz_type = schwarz_type;
-	
 	schwarz->memt = memt;
 	schwarz->mask = mask;
 	schwarz->maxa = maxa;
+
+#if DEBUG_MODE
+    printf("### DEBUG: fasp_schwarz_setup ...... [Finish]\n");
+#endif
 	
-	return flag;
-	
+	return flag;	
 }
- 
- 
- 
- 
- 
- 
+
+/*---------------------------------*/
+/*--        End of File          --*/
+/*---------------------------------*/
