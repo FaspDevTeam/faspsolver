7acbb91a35cb13d574922e35ee881d90cdf75c99
Feng Chunsheng <xtu.fengchunsheng@gmail.com>
2012-06-26 00:19:34 +0800

    adding threads_shedule.c

--- base/src/threads_schedule.c
+++ base/src/threads_schedule.c
@@ -1,5 +1,6 @@
-/*! \file get_num_threads.c 
- *  \brief Get and set number of threads for OMP.
+/*! \file threads_schedule.c 
+ *  \brief Get and set number of threads for OMP, 
+ *         besides, assigning the load to each thread involed.
  */
 
 #include <stdio.h>
@@ -22,7 +23,8 @@
  * \author Chunsheng Feng, Xiaoqiang Yue and Zheng Li
  * \date   June/15/2012
  */
- 
+
+#if FASP_USE_OPENMP
 INT thread_ini_flag = 0;
 
 INT FASP_GET_NUM_THREADS()
@@ -53,6 +55,43 @@ INT Fasp_Set_Num_Threads(INT nthreads )
 	omp_set_num_threads( nthreads );
 
 	return nthreads;
+}
+
+#endif
+
+
+/**
+ * \fn     void FASP_GET_START_END(ITN procid, INT nprocs, INT n, INT *start, INT *end)
+ *
+ * \brief  Assign Load to each thread.
+ *
+ * \param procid Index of thread
+ * \param nprocs Number of threads
+ * \param n      Total workload
+ * \param start  Pointer to the begin of each thread in total workload
+ * \param end    Pointer to the end of each thread in total workload
+ *
+ * \author Chunsheng Feng, Xiaoqiang Yue and Zheng Li
+ * \date   June/25/2012
+ */
+
+void FASP_GET_START_END(INT procid, INT nprocs, INT n, INT *start, INT *end)
+{
+	INT chunk_size = n / nprocs;
+	INT mod =  n % nprocs;
+	INT start_loc, end_loc;
+
+	if( procid < mod) {
+		end_loc = chunk_size + 1;
+		start_loc = end_loc * procid;
+	}
+	else {
+		end_loc = chunk_size;
+		start_loc = end_loc * procid + mod;
+	}
+	end_loc = end_loc + start_loc;
+	*start = start_loc;
+	*end = end_loc;
 }
 
 /*---------------------------------*/
