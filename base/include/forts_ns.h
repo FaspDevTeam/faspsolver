859d41713240bf577c82b217a6e353afcb0c36ca
Chensong Zhang <zhangchensong@gmail.com>
2012-07-04 15:46:42 +0800

    Ziteng and Chensong: Fix a VC++ compability problem. New Windows version is almost ready.

--- base/include/forts_ns.h
+++ base/include/forts_ns.h
@@ -1,3 +1,25 @@
+#ifdef __cplusplus 
+extern "C" {void cut0_(INT *n,INT *ia,INT *ja,REAL *a,INT *iaw,INT *jaw,INT *jblk,INT *iblk,INT *nblk,INT *lwork1,INT *lwork2,INT *lwork3,INT *msize);}
+extern "C" {void chsize_(REAL *a,REAL *b,REAL *tol,INT *imin);}
+extern "C" {void shift_(INT *nxadj,INT *nadj,INT *n);}
+extern "C" {void dfs_(INT *n,INT *ia,INT *ja,INT *nblk,INT *iblk,INT *jblk,INT *lowlink,INT *iedge,INT *numb);}
+extern "C" {void permat_(INT *iord,INT *ia,INT *ja,REAL *an,INT *n,INT *m,INT *iat,INT *jat,REAL *ant);}
+extern "C" {void pervec_(INT *iord,REAL *u1,REAL *u2,INT *n);}
+extern "C" {void perback_(INT *iord,REAL *u1,REAL *u2,INT *n);}
+extern "C" {void perm0_(INT *iord,INT *ia,INT *ja,REAL *an,INT *n,INT *m,INT *iat,INT *jat,REAL *ant);}
+extern "C" {void icopyv_(INT *iu,INT *iv,INT *n);}
+extern "C" {void mxfrm2_(INT *n,INT *ia,INT *ja,INT *nblk,INT *iblock,INT *jblock,INT *mask,INT *maxa,INT *memt,INT *maxbs);}
+extern "C" {void sky2ns_(INT *n,INT *ia,INT *ja,REAL *a,INT *nblk,INT *iblock,INT *jblock,INT *mask,INT *maxa,REAL *au,REAL *al);}
+extern "C" {void fbgs2ns_(INT *n,INT *ia,INT *ja,REAL *a,REAL *x,REAL *b,INT *nblk,INT *iblock,INT *jblock,INT *mask,INT *maxa,REAL *au,REAL *al,REAL *rhsloc,INT *memt);}
+extern "C" {void bbgs2ns_(INT *n,INT *ia,INT *ja,REAL *a,REAL *x,REAL *b,INT *nblk,INT *iblock,INT *jblock,INT *mask,INT *maxa,REAL *au,REAL *al,REAL *rhsloc,INT *memt);}
+extern "C" {void doluns_(REAL *au,REAL *al,INT *maxa,INT *nn);}
+extern "C" {void sluns_(REAL *au,REAL *al,REAL *v,INT *maxa,INT *nn);}
+extern "C" {void dolu_(REAL *a,INT *maxa,INT *nn);}
+extern "C" {void slvlu_(REAL *a,REAL *v,INT *maxa,INT *nn);}
+extern "C" {void ijacrs_(INT *ln,INT *ia,INT *ja,REAL *a,INT *n,INT *nnz,INT *ir,INT *ic,REAL *aij);}
+extern "C" {void sympat_(INT *ln,INT *ia,INT *ja,INT *n,INT *ir,INT *ic,REAL *aij);}
+extern "C" {void levels_(INT *inroot,INT *ia,INT *ja,INT *mask,INT *nlvl,INT *iblock,INT *jblock,INT *maxlev);}
+#else
 extern void cut0_(INT *n,INT *ia,INT *ja,REAL *a,INT *iaw,INT *jaw,INT *jblk,INT *iblk,INT *nblk,INT *lwork1,INT *lwork2,INT *lwork3,INT *msize);
 extern void chsize_(REAL *a,REAL *b,REAL *tol,INT *imin);
 extern void shift_(INT *nxadj,INT *nadj,INT *n);
@@ -18,3 +40,4 @@ extern void slvlu_(REAL *a,REAL *v,INT *maxa,INT *nn);
 extern void ijacrs_(INT *ln,INT *ia,INT *ja,REAL *a,INT *n,INT *nnz,INT *ir,INT *ic,REAL *aij);
 extern void sympat_(INT *ln,INT *ia,INT *ja,INT *n,INT *ir,INT *ic,REAL *aij);
 extern void levels_(INT *inroot,INT *ia,INT *ja,INT *mask,INT *nlvl,INT *iblock,INT *jblock,INT *maxlev);
+#endif
