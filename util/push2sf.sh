d9b94517b37967be09c9048e17a51aa0287dbada
Chensong Zhang <zhangchensong@gmail.com>
2013-02-15 18:05:50 +0800

    Fix a path bug in test/Makefile and push2sf.sh.

--- util/push2sf.sh
+++ util/push2sf.sh
@@ -7,7 +7,11 @@ USERID="faspsolver"
 # optional parameters
 OPT="--delete --progress --times --exclude *~"
 
-# upload: Should be ran in doc directory with html as a subdir
+# upload: Should be ran in doc directory with htdocs as a subdir
+if [ ! -d htdocs/download ]; then
+    mkdir htdocs/download
+fi
+
 cp ../../download/.ht* htdocs/download
 cp ../../download/*.zip htdocs/download
 cp ../../download/*.pdf htdocs/download
