fb2c876bbe70c232823d378c548bdff315799615
Chensong Zhang <zhangchensong@gmail.com>
2013-09-21 10:02:36 +0800

    Modify VS08 dependency setup.

--- util/stat.sh
+++ util/stat.sh
@@ -2,6 +2,6 @@
 
 ## This shell command generates statistics of FASP files in the ./log/FILES.
 
-wc -l ./base/src/*.c ./base/src/*.inl ./base/include/*.h > ./log/FILES
+wc -l ./base/include/*.h ./base/src/*.f ./base/src/*.c ./base/src/*.inl > ./log/FILES
 
 
