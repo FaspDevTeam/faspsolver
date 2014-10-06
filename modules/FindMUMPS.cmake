06d690e308aeac15d5c6274f125d717ca068dc66
Chensong Zhang <zhangchensong@gmail.com>
2014-10-06 15:41:07 +0800

    Fix MUMPS dependence check for cmake

--- modules/FindMUMPS.cmake
+++ modules/FindMUMPS.cmake
@@ -1,31 +1,66 @@
 # - Try to find MUMPS
+#
 # Once done this will define
 #  MUMPS_FOUND - System has Mumps
 #  MUMPS_INCLUDE_DIRS - The Mumps include directories
 #  MUMPS_LIBRARY_DIRS - The library directories needed to use Mumps
 #  MUMPS_LIBRARIES    - The libraries needed to use Mumps
+#
+#  Chensong Zhang
+#  10/06/2014
+
+message(STATUS "Checking for dependent packages of 'MUMPS'")
+
+find_package(BLAS)
 
 if (MUMPS_INCLUDE_DIR)
   # in cache already
   SET(MUMPS_FIND_QUIETLY TRUE)
 endif (MUMPS_INCLUDE_DIR)
 
-find_path(MUMPS_INCLUDE_DIR NAMES dmumps_c.h
-     PATHS "$ENV{IPOPT_HOME}/ThirdParty/Mumps/MUMPS/include/"
-           "/usr/include/" )
-   
-find_library( MUMPS_LIBRARY 
-              dmumps
-              PATHS "$ENV{IPOPT_HOME}/lib/"
-                    "/usr/lib" )
+# Check for header file
+find_path(MUMPS_INCLUDE_DIR dmumps_c.h
+ HINTS ${MUMPS_DIR}/include $ENV{MUMPS_DIR}/include $ENV{IPOPT_HOME}/MUMPS/include/ /usr/include/ 
+ DOC "Directory where the MUMPS header is located"
+ )
+mark_as_advanced(MUMPS_INCLUDE_DIR)
+
+# Check for MUMPS libraries: dmumps, mumps_common pord mpiseq
+find_library(MUMPS_LIB dmumps
+  HINTS ${MUMPS_DIR}/lib $ENV{MUMPS_DIR}/lib $ENV{IPOPT_HOME}/MUMPS/lib/ /usr/lib/
+  DOC "The MUMPS libraries"
+  )
+mark_as_advanced(MUMPS_LIB)
+
+find_library(MUMPS_LIB_COMMON mumps_common
+  HINTS ${MUMPS_DIR}/lib $ENV{MUMPS_DIR}/lib $ENV{IPOPT_HOME}/MUMPS/lib/ /usr/lib/
+  DOC "The MUMPS libraries"
+  )
+mark_as_advanced(MUMPS_LIB_COMMON)
+
+find_library(MUMPS_LIB_PORD pord
+  HINTS ${MUMPS_DIR}/lib $ENV{MUMPS_DIR}/lib ${MUMPS_DIR}/PORD/lib $ENV{MUMPS_DIR}/PORD/lib
+  DOC "The MUMPS PORD libraries"
+  )
+mark_as_advanced(MUMPS_LIB_PORD)
+
+find_library(MUMPS_LIB_MPISEQ mpiseq
+  HINTS ${MUMPS_DIR}/lib $ENV{MUMPS_DIR}/lib ${MUMPS_DIR}/libseq $ENV{MUMPS_DIR}/libseq
+  DOC "The MUMPS MPISEQ libraries"
+  )
+mark_as_advanced(MUMPS_LIB_MPISEQ)
 
 set(MUMPS_INCLUDE_DIRS "${MUMPS_INCLUDE_DIR}" )
-set(MUMPS_LIBRARIES "${MUMPS_LIBRARY}" )
+set(MUMPS_LIBRARIES "${MUMPS_LIB}" "${MUMPS_LIB_COMMON}" "${MUMPS_LIB_PORD}" "${MUMPS_LIB_MPISEQ}")
+
+if (BLAS_FOUND)
+  set(MUMPS_LIBRARIES ${MUMPS_LIBRARIES} ${BLAS_LIBRARIES})
+endif()
 
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set LIBCPLEX_FOUND to TRUE
 # if all listed variables are TRUE
 find_package_handle_standard_args(MUMPS  DEFAULT_MSG
-                                  MUMPS_LIBRARY MUMPS_INCLUDE_DIR)
+                                  MUMPS_LIB MUMPS_INCLUDE_DIR)
 
-mark_as_advanced(MUMPS_INCLUDE_DIR MUMPS_LIBRARY)
+mark_as_advanced(MUMPS_INCLUDE_DIR MUMPS_LIB)
