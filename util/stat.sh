71947fbffefa7423ec1a56ba5adf18c5524763f4
Chensong Zhang <zhangchensong@gmail.com>
2015-04-08 17:50:44 +0800

    Small changes on structure.

--- util/stat.sh
+++ util/stat.sh
@@ -2,6 +2,6 @@
 
 ## This shell command generates statistics of FASP files in the ./log/FILES.
 
-wc -l ./base/include/*.h ./base/src/*.f ./base/src/*.c ./base/src/*.inl > ./log/FILES
+wc -l ./base/include/*.h ./base/src/*.inl ./base/src/*.f ./base/src/*.c > ./log/FILES
 
 
