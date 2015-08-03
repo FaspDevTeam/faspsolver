8d7aca705d54bd1f00cf677b9a89a51785cf8b3d
Ludmil T Zikatanov <ltzmultigrid@gmail.com>
2015-08-03 12:51:27 +0800

    cmakelists work

--- FASP_init.cmake
+++ FASP_init.cmake
@@ -1,15 +1,3 @@
-# Helper modules.
-include(CheckFunctionExists)
-include(CheckIncludeFile)
-
-set(CMAKE_VERBOSE_MAKEFILE 1) 
-set(GDB 1 CACHE BOOL "debugging or not")
-set(OPENMP 0 CACHE BOOL "Openmp use")
-set(USE_MUMPS 0 CACHE BOOL "MUMPS use")
-
-# For which compilers we shall search (if none found then the 
-# default cmake compiler detection will be invoked. 
-	set(F_C "dragonegg-3.4-gfortran-mp-4.6")
         if( THE_C AND C_XX AND F_C )
 	     find_program(THE_CXX ${C_XX} )
 	     find_program(THE_F ${F_C} )
