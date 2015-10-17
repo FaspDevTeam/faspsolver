c4756f506acc0286aec04dbea0274322da4c6982
Ludmil T Zikatanov <ludmil@zsolvers.net>
2015-10-17 15:00:05 -0700

    updated all makefiles in test and tutorial Created a new file "Config.mk" which can be loaded from test/Makefile and tutorial/Makefile and contains FC, CC and so with values as during the library build

---
+++ modules/write_config.cmake
@@ -0,0 +1,23 @@
+#  
+#  Writing configuration so that it can be used by regular makefiles 
+#
+#  Modified   20151017   --ltz
+###################################################################
+set(CONFIGMK ${FASP_SOURCE_DIR}/Config.mk)
+message("-- Writing FASP configuration to ${CONFIGMK}")
+file(WRITE  ${CONFIGMK} 
+"
+\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\# Automatically generated \#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#
+\# Fast Auxiliary Space Preconditioners (FASP) 
+\#
+\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#
+\# This file is rewritten when \"make config\" is run.
+\# It is (s)included by test/Makefile and tutorial/Makefile
+\#
+\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#
+fasp_prefix=${FASP_INSTALL_PREFIX}
+CC=${CMAKE_C_COMPILER}
+CXX=${CMAKE_CXX_COMPILER}
+FC = ${CMAKE_Fortran_COMPILER}
+\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#
+")
