39870e37157ba84c743fbf66b522a29b5727f43a
Ludmil T Zikatanov <ludmil@zsolvers.net>
2015-10-17 23:48:13 -0700

    changed makefiles to check for library presence ; changed top-level makefile to do a make-install before it does make test or make tutorial

--- modules/write_config.cmake
+++ modules/write_config.cmake
@@ -5,6 +5,15 @@
 ###################################################################
 set(CONFIGMK ${FASP_SOURCE_DIR}/Config.mk)
 message("-- Writing FASP configuration to ${CONFIGMK}")
+# determinining library name; this is  a bit hard coding here
+if(SHARED)
+    set(FASP_LIBRARY_PREFIX ${CMAKE_SHARED_LIBRARY_PREFIX})
+    set(FASP_LIBRARY_SUFFIX ${CMAKE_SHARED_LIBRARY_SUFFIX})
+else(SHARED)
+    set(FASP_LIBRARY_PREFIX ${CMAKE_STATIC_LIBRARY_PREFIX})
+    set(FASP_LIBRARY_SUFFIX ${CMAKE_STATIC_LIBRARY_SUFFIX})
+endif(SHARED)
+
 file(WRITE  ${CONFIGMK} 
 "
 \#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\# Automatically generated \#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#
@@ -16,6 +25,7 @@ file(WRITE  ${CONFIGMK}
 \#
 \#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#
 fasp_prefix=${FASP_INSTALL_PREFIX}
+fasp_library=${FASP_LIBRARY_PREFIX}${FASP_LIBRARY_NAME}${FASP_LIBRARY_SUFFIX}
 CC=${CMAKE_C_COMPILER}
 CXX=${CMAKE_CXX_COMPILER}
 FC=${CMAKE_Fortran_COMPILER}
