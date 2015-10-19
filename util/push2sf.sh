ff1ebc9338a80f16dd92d24e10669b2608b22377
Chensong Zhang <zhangchensong@gmail.com>
2015-10-19 09:09:09 +0800

    Archive vesion 1.8.2 and remove passwd from fasp.sf.net

--- util/push2sf.sh
+++ util/push2sf.sh
@@ -12,7 +12,7 @@ if [ ! -d htdocs/download ]; then
     mkdir htdocs/download
 fi
 
-cp ../../download/.ht* htdocs/download
+# cp ../../download/.ht* htdocs/download
 cp ../../download/*.zip htdocs/download
 cp ../../download/*.pdf htdocs/download
 rsync -avz $1 ${OPT} ./htdocs ${USERID}@${SERVER}:/home/project-web/fasp
