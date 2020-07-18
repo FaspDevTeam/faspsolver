817bca9e2a7a92decc8f97824570fb57070c4144
zhangcs <zhangchensong@gmail.com>
2020-07-18 16:24:13 +0800

    Update test example for Krylov

--- test/main/testkrylov.c
+++ test/main/testkrylov.c
@@ -36,29 +36,30 @@ int main(int argc, char **argv) {
     x.row = n;
 
     b.val = (REAL *) malloc(n * sizeof(REAL));
-    x.val = (REAL *) malloc(n * sizeof(REAL));
-
     for (int j = 0; j < n; ++j) b.val[j] = 1.0;
-
-    for (int j = 0; j < n; ++j) x.val[j] = 1.0;
 
     const REAL  tol     = 1e-5;
     const INT   maxit   = 500;
     const INT   restart = 10;
-    const SHORT prtlvl  = PRINT_NONE;
+    const SHORT prtlvl  = PRINT_MIN;
     REAL start;
 
+    x.val = (REAL *) malloc(n * sizeof(REAL)); // solution vector
+
     printf("Test CG method ...\n");
+    for (int j = 0; j < n; ++j) x.val[j] = 1.0; // initial guess
     start = clock();
     fasp_solver_dcsr_pcg(&A, &b, &x, NULL, tol, maxit, 1, prtlvl);
     printf("CPU Time: %lf seconds\n", (clock() - start) / CLOCKS_PER_SEC );
 
     printf("Test GMRES method ...\n");
+    for (int j = 0; j < n; ++j) x.val[j] = 1.0; // initial guess
     start = clock();
     fasp_solver_dcsr_pgmres(&A, &b, &x, NULL, tol, maxit, restart, 1, prtlvl);
     printf("CPU Time: %lf seconds\n", (clock() - start) / CLOCKS_PER_SEC );
 
     printf("Test vGMRES method ...\n");
+    for (int j = 0; j < n; ++j) x.val[j] = 1.0; // initial guess
     start = clock();
     fasp_solver_dcsr_pvgmres(&A, &b, &x, NULL, tol, maxit, restart, 1, prtlvl);
     printf("CPU Time: %lf seconds\n", (clock() - start) / CLOCKS_PER_SEC );
